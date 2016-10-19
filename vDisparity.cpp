dc0fed168594f836932c695f1e4778c95fe23192
Daniel Hernandez-Juarez <dhernandez0@gmail.com>
2016-10-19 18:09:14 +0200

    License added

--- vDisparity.cpp
+++ vDisparity.cpp
@@ -1,3 +1,23 @@
+/**
+    This file is part of show_stixels. (https://github.com/dhernandez0/show_stixels).
+
+    Copyright (c) 2016 Daniel Hernandez Juarez.
+
+    show_stixels is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    show_stixels is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with show_stixels.  If not, see <http://www.gnu.org/licenses/>.
+
+**/
+
 #include "vDisparity.h"
 
 // OpenCV
